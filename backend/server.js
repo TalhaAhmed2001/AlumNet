6f5cc50da08ec2d1fdd3d99cc4c7ea1ca015d792
TalhaAhmed2001 <120246152+TalhaAhmed2001@users.noreply.github.com>
2024-05-11 01:14:36 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use(bodyParser.json({ limit: '50mb' }));
